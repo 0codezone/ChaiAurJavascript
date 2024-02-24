9d6fdb7bc87dc90a86ae8cfabbf0b778cfcdfc87
Mohit Darji <100986394+0codezone@users.noreply.github.com>
2024-02-24 19:55:40 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,37 @@
+// console.log(2>1); //true
+// console.log(2<1); //false
+// console.log(2>=1); //true
+// console.log(2<=1); //false
+// console.log(2!=1); //true
+
+// console.log(2=1); //invalid left hand side assignment
+// console.log(2==1); //false
+// console.log(2===1); //false
+
+// console.log(2==2); //true
+// console.log(2===2); //true
+// console.log(2=="2"); //true
+// console.log(2==="2"); //false  === also check datatype 
+
+// console.log("2" > 1); //true
+// console.log("2" > 2); //false
+// console.log("2" >= 2) //true
+// console.log("02" > 1); //true
+// console.log("a2" > 1); //false
+// console.log(typeof("a2" > 1));  Boolean
+
+// console.log(null > 0);  //false
+// console.log(null < 0);  //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true   comparision convert null to a number , trating as 0
+// console.log(null <= 0);  //true
+
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined <= 0); //false
+console.log(undefined >= 0); //false
+
+
+////////// strict check (===)
