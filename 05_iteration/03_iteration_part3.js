2821e2a8408689f0c5a2ba30ad98c9728828deec
Mohit Darji <100986394+0codezone@users.noreply.github.com>
2024-03-02 10:18:44 +0000

    foreach and examples

---
+++ 05_iteration/03_iteration_part3.js
@@ -0,0 +1,36 @@
+// ---------------------------------
+// for in ------------------------------------
+//  ------------------------------------
+
+// const myObject = {
+//     "js" :   "javascript",
+//     "py" : "python",
+//     "rb" : "ruby",
+//     "java" : "java"
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key}  : ${myObject[key]} `);
+// }
+
+// -----------
+
+// const programming = ["js", "py", "swift", "java"]
+// for (const key in programming) {
+//    console.log(`for key ${key} value is => ${programming[key]}`);
+// }
+
+// ---------------------------------
+// for each ------------------------------------
+//  ------------------------------------
+// const packet = [1,23,4,5,6,78,8]
+// packet.forEach((item)=>{
+//    console.log(item);
+// })
+
+// const packet = [1,23,4,5,6,78,8]
+// packet.forEach((item, index, arr)=>{
+//    console.log(item, index, arr);
+// })
+
+// NOTE : for-each kuch bhi return nhi krta h tum try krte ho toh vo undefiend hi dega
