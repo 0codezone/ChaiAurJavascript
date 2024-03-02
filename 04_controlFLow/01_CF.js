7f696e0e9fc56c6d2e627b0625f0452ce3e62bcd
Mohit Darji <100986394+0codezone@users.noreply.github.com>
2024-03-02 09:50:12 +0000

    control flow if,  else if ...

---
+++ 04_controlFLow/01_CF.js
@@ -0,0 +1,50 @@
+//##------------------------ Conditional Statements (if...else, switch):
+let x = 10;
+
+// if (x<11) {
+//     console.log("yes iits less ");
+// }
+
+
+// if (x > 5) {
+//     console.log("x is greater than 5");
+// } else {
+//     console.log("x is not greater than 5");
+// }
+
+
+// let fruit = "apple";
+// switch (fruit) {
+//     case "banana":
+//         console.log("It's a banana.");
+//         break;
+//     case "apple":
+//         console.log("It's an apple.");
+//         break;
+//     default:
+//         console.log("Unknown fruit.");
+// }
+
+
+
+
+
+
+// ##------------------------- Exception Handling (try...catch...finally):
+
+// try {
+//     let result = someFunction(); // calling a function that may throw an error
+//     console.log(result);
+// } catch (error) {
+//     console.error("An error occurred:", error);
+// }
+
+
+// try {
+//     let result = someFunction();
+//     console.log(result);jk
+// } catch (error) {
+//     console.error("An error occurred:", error);
+// } finally {
+//     console.log("This block always executes, regardless of errors.");
+// }
